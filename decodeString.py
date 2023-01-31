bc1d05f2021498bfa1fd241143bb22f09ffb188d
Fuad <fuadmiftah9@gmail.com>
2023-01-31 10:04:13 +0300

    6thcom

--- decodeString.py
+++ decodeString.py
@@ -1,36 +1,21 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
-        def decoder(n,s):
-            if stack[n] == '[':
-                return s
-            popped = stack.pop(n)
-            s = s + popped[::-1]
+        it = iter(s)
+        return self.recursiveImplement(s, it)
 
-            return decoder(n-1,s)
-        
-        for word in s:
-            if word != ']':
-                if word.isdigit():
-                    num = word
-                    while stack!= [] and stack[-1].isdigit():
-                        num = stack.pop() + num
-                    stack.append(num)
-
-                else:
-                    stack.append(word)
-            
+    def recursiveImplement(self, s: str, it) -> Tuple:
+        ret, times = [], 0
+        for c in it:
+            if c.isdigit():
+                times = 10*times + int(c)
+            elif c is '[':
+                ret.append(times * self.recursiveImplement(s,it))
+                times = 0
+            elif c is ']':
+                break
             else:
-                s = decoder(len(stack) - 1,'')
-                s = s[::-1]
-                stack.pop()
-                multi = int(stack.pop())
-                for i in range(multi):
-                    stack.append(s)
-        
-        res = ''.join(stack)
-        #print(res)
-        return res
+                ret.append(c)
+        return "".join(ret)
         
 
 s = "3[a]2[bc]"
