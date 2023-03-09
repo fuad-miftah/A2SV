2ae7cf303989701490cdf5b9999bfca5dbc3ecc6
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-03-09 09:33:24 +0300

    added running-sum-of-1d-array.python

---
+++ running-sum-of-1d-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+        if nums == []:
+            return []
+        prefix = [nums[0]]
+        for i in range(1,len(nums)):
+            prefix.append(prefix[i-1]+nums[i])
+        return prefix
