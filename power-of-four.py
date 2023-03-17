ca81a391360fda7e39921885c078e5ba59dc2122
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-03-17 22:23:51 +0300

    added power-of-four.python

--- power-of-four.py
+++ power-of-four.py
@@ -1,16 +1,10 @@
-class Solution(object):
-    def isPowerOfFour(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        
-        if n == 1: 
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
             return True
-        if n <= 0 : 
+        if n < 1:
             return False
-        if n % 4 == 0:
-            n = int(n/4)
-            return self.isPowerOfFour(n)
-        else:
+        if n % 4 != 0:
             return False
+        n = n / 4
+        return self.isPowerOfFour(n)
