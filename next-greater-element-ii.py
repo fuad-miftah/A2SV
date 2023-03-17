33902c8e3d30e8bbf3b34ad685ba43f68fb1b349
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-03-17 22:19:17 +0300

    added next-greater-element-ii.python

--- next-greater-element-ii.py
+++ next-greater-element-ii.py
@@ -2,21 +2,11 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         stack = []
         res = [-1]*(len(nums))
-        for i in range(len(nums)):
-            if stack:
-                while stack and stack[-1][1] < nums[i]:
-                    index,val = stack.pop()
+        for j in range(2):
+            for i in range(len(nums)):
+                while stack and nums[stack[-1]] < nums[i]:
+                    index = stack.pop()
                     res[index] = nums[i]
-                stack.append([i,nums[i]])
-            else:
-                stack.append([i,nums[i]])
-        for i in range(len(nums)):
-            if stack:
-                while stack and stack[-1][1] < nums[i]:
-                    index,val = stack.pop()
-                    res[index] = nums[i]
-                stack.append([i,nums[i]])
-            else:
-                stack.append([i,nums[i]])
+                stack.append(i)
        
         return res
