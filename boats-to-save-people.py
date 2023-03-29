6f54137a8fbbc49ae766d0607622a004181c5f88
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-03-29 11:46:13 +0300

    added boats-to-save-people.python

---
+++ boats-to-save-people.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left = 0
+        right = len(people)-1
+        output = 0
+        while right >= left:
+            if people[right] + people[left] <= limit:
+                right-=1
+                left+=1
+            else:
+                right-=1
+            output+=1
+        return output
