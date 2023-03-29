3b19c53c6f9027fe08de40e3a49acab75bd6e269
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-03-29 11:50:01 +0300

    added max-number-of-k-sum-pairs.python

---
+++ max-number-of-k-sum-pairs.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        res = 0
+        d = {}
+        for x in nums:
+            t = k-x
+            if t in d and d[t] > 0:
+                res += 1
+                d[t] -= 1
+            else:
+                if x not in d:
+                    d[x] = 0
+                d[x] += 1
+        return(res)
