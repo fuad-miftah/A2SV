ed2cddaed94ae2d67d0b88d0a80532c1988627fe
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-04-07 14:04:40 +0300

    added find-center-of-star-graph.python

---
+++ find-center-of-star-graph.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        first = edges[0][0]
+        second = edges[0][1]
+
+        if first == edges[1][0] or first == edges[1][1]:
+            return first
+        else:
+            return second
