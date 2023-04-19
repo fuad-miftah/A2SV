d9f360b6ae31859cefc409a6ca1006b4c2e0cef3
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-04-19 06:37:04 +0300

    added top-k-frequent-elements.python

--- top-k-frequent-elements.py
+++ top-k-frequent-elements.py
@@ -1,17 +1,37 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        counts = collections.Counter(nums)
-        result = sorted(nums, key=lambda x: (counts[x], x), reverse=True)
-        output = []
-        i = 0
-        while len(output) < k:
-            if result[i] not in output:
-                output.append(result[i])
-            i+=1
-    
-        return output
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        unique = list(count.keys())
+        
+        def partition(left, right, pivot_index) -> int:
+            pivot_frequency = count[unique[pivot_index]]
+            
+            store_index = left
+            for i in range(left, right):
+                if count[unique[i]] < pivot_frequency:
+                    unique[store_index], unique[i] = unique[i], unique[store_index]
+                    store_index += 1
+
+            unique[right], unique[store_index] = unique[store_index], unique[right]  
+            
+            return store_index
+        
+        def quickselect(left, right, k_smallest) -> None:
+
+            if left == right: 
+                return
+            
+            pivot_index = right    
+            pivot_index = partition(left, right, pivot_index)
+            
+            if k_smallest == pivot_index:
+                 return 
+            elif k_smallest < pivot_index:
+                quickselect(left, pivot_index - 1, k_smallest)
+            else:
+                quickselect(pivot_index + 1, right, k_smallest)
+         
+        n = len(unique) 
+  
+        quickselect(0, n - 1, n - k)
+        return unique[n - k:]
