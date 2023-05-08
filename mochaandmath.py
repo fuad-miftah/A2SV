f6546ca57484c6776fd5c39f899dd8ef4c662901
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-05-08 22:31:03 +0300

    added mochaandmath.py

---
+++ mochaandmath.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+ 
+    res = arr[0]
+    for i in range(1,n):
+        res = res & arr[i]
+ 
+    print(res)
