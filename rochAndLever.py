767ef83ea3e6a46e230c85fdd4a9d9df2e198f34
fuad-miftah <85777124+fuad-miftah@users.noreply.github.com>
2023-05-08 22:17:07 +0300

    added rochAndLever.py

---
+++ rochAndLever.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    counter = {}
+    ans = 0
+    for i in arr:
+        counter[len(bin(i)) - 2] = 1 + counter.get(len(bin(i)) - 2, 0)
+        ans += counter[len(bin(i)) - 2] - 1
+    print(ans)
