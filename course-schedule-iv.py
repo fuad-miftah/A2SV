41fbf73520b0129c97525c2302a6d1086d6cdfa2
Fuad Miftah Nasir <85777124+fuad-miftah@users.noreply.github.com>
2023-10-05 18:50:36 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,31 +1,17 @@
 class Solution:
-    def bfs(self,i,dic,v):
-        queue = deque()
-        queue.append(i)
-        visited = set()
-        visited.add(i)
-        while queue:
-            node = queue.popleft()
-            for child in dic[node]:
-                if child == v:
-                    return True
-                if child not in visited:
-                    queue.append(child)
-                    visited.add(child)
-        return False
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        n = numCourses
+
+        dist = [[False for _ in range(n)] for _ in range(n) ]
+        for r,t in prerequisites:
+            dist[r][t] = True 
         
-        dic = defaultdict(list)
-        for c,p in prerequisites:
-            dic[c].append(p)
-
+        for k in range(n):
+            for j in range(n):
+                for i in range(n):
+                    dist[i][j] |= (dist[i][k] and  dist[k][j])
         res = []
-
-        for u,v in queries:
-            p = self.bfs(u,dic,v)
-            if p:
-                res.append(True)
-            else:
-                res.append(False)
-                
+        for x,y in queries:
+            res.append(dist[x][y])
+        
         return res
