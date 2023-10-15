60f4eb7f25c412d426a2b1e3e9382abd593577a1
Fuad Miftah Nasir <85777124+fuad-miftah@users.noreply.github.com>
2023-10-15 21:39:02 +0300

    added maximum-xor-of-two-numbers-in-an-array.python

---
+++ maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        newNums = []
+        for i in range(len(nums)):
+            newNums.append(bin(nums[i])[2:].zfill(32))
+        mask = 0
+        ans = 0
+        for i in range(31,-1,-1):
+            store = set()
+            mask |= (1<<i)
+            for num in nums:
+                store.add(num & mask)
+            temp = ans | (1<<i)
+            for val in store:
+                if val ^ temp in store:
+                    ans |= (1<<i)
+                    break
+        return ans
